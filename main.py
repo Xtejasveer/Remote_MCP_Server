480df924388e0fc941bb6cf3028c96a345dd6543
Tejasveer Singh Sodhi <tejasveer2020@gmail.com>
2026-07-22 13:50:34 +0530

    Remote server

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
-def main():
-    print("Hello from remote-mcp!")
+from fastmcp import FastMCP
+import random 
+import json
 
+mcp = FastMCP("Simple Calculator server")
 
+# Tool : Add two numbers
+@mcp.tool
+def add(a:int, b:int) -> int:
+    """Add two numbers together.
+    Args:
+        a : First Number
+        b : Second Number
+
+    Returns:
+        The sum of a and b
+    """
+    return a+b
+
+# Tool : Generate a random number
+@mcp.tool
+def random_number(min_val : int = 1, max_val : int =100)-> int :
+    """Generate a random number within a range.
+    
+    Args:
+        min_val : Minumum value (default :1)
+        max_val : Maximum value (default :100)
+
+    Returns:
+        A random integer between min_val and max_val 
+    """
+
+    return random.randint(min_val, max_val)
+
+@mcp.resource("info://server")
+def server_info() -> str:
+    """Get information about this server."""
+    info = {
+        "name" : "Simple Calculator Server",
+        "version" : "1.0.0",
+        "description" : "A basic MCP server with math tools",
+        "tools" : ["add", "random_number"],
+        "author" : "Tejasveer Singh Sodhi"
+    }
+    return json.dumps(info, indent=2)
+
+## start the server
 if __name__ == "__main__":
-    main()
+    mcp.run(transport="http", host = "0.0.0.0", port = 8000)
